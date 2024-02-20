ab30b20cd6450da69248c37e723d6a7eeea62b3b
Justin Rentie <jrentie@talentreef.com>
2024-02-20 14:47:35 -0700

    added description and read more button

--- src/components/utility/SuperheroGrid.js
+++ src/components/utility/SuperheroGrid.js
@@ -2,7 +2,7 @@ import './SuperheroGrid.css'
 
 import React, { useEffect, useState } from 'react'
 import { Card, CardMedia, CardContent, Typography, Pagination } from '@mui/material'
-import { motion, useCycle } from 'framer-motion'
+import { motion, useCycle, useAnimate } from 'framer-motion'
 
 import styled from 'styled-components'
 
@@ -29,6 +29,8 @@ function SuperheroGrid () {
     const [isLoading, setIsLoading] = useState(true)
     const [pages, setPages] = useState(0)
     const [offset, setOffset] = useState(0)
+
+    const [scope, animateBlock] = useAnimate();
 
     const limit = 24
 
@@ -63,33 +65,62 @@ function SuperheroGrid () {
         }, 1100)
     }
 
+    const handleAnimate = async (i) => {
+        animateBlock("#styled-card-" + i, {scale: 2})
+        document.getElementById("card-media-" + i).height = 50
+        document.getElementById("name-" + i).style.fontSize = "9px"
+        document.getElementById("description-" + i).style.display = "block"
+        document.getElementById("read-more-button-" + i).style.display = "flex"
+    }
+
+    const handleHoverEnd = (i) => {
+        document.getElementById("card-media-" + i).height = 120
+        document.getElementById("name-" + i).style.fontSize = "16px"
+        document.getElementById("description-" + i).style.display = "none"
+        document.getElementById("read-more-button-" + i).style.display = "none"
+    }
+
+    const truncate = (str, n) =>{
+        return (str.length > n) ? str.slice(0, n-1) + '...' : str;
+    }
+
     return(
         <>
             <motion.div 
                 initial={{opacity:0, x: 100}}
                 animate={animate}
                 className="SuperheroGrid"
+                ref={scope}
             >
                 {!isLoading && characters.data.results.map((character, i) => {
                     return (
                         <StyledCard
+                            id={"styled-card-" + i}
                             component={motion.div}
-
                             whileHover={{
                                 scale: 1.1,
                                 transition: { duration: 0.3 }
                             }}
+                            onTap={() => handleAnimate(i)}
+                            onHoverEnd={()=> handleHoverEnd(i)}
                         >
                             <CardMedia 
+                                id={"card-media-" + i}
                                 component="img"
                                 wide
                                 image={character.thumbnail.path + "." + character.thumbnail.extension }
                                 height={120}
                             />
                             <CardContent>
-                                <Typography gutterBottom variant="p" component="p">
+                                <Typography id={"name-" + i}   gutterBottom variant="p" component="p">
                                 {character.name}
                                 </Typography>
+                                <div>
+                                    <p id={"description-" + i} className="description" >
+                                        {(character.description === "") ? "This character was not provided a description from the marvel API." : truncate(character.description, 200)}
+                                    </p>
+                                </div>
+                                <button id={"read-more-button-" + i} className="read-more-button">Read More</button>
                             </CardContent>
                         </StyledCard>
                     )
